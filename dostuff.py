2545c69c1161ac34982680a68827887c701e5b7b
Frank Hu <jinfanfrank@gmail.com>
2025-02-24 15:10:04 -0500

    added scores

--- dostuff.py
+++ dostuff.py
@@ -7,13 +7,13 @@ addscore(first, last, place)
 emailscores(note)
 '''
 
-task = '''For your fourth task, take a picture with your partner and one of our class advisors, Char!
-Hint: She's usually in CAMD.'''
+#task = '''For your fourth task, take a picture with your partner and one of our class advisors, Char!
+#Hint: She's usually in CAMD.'''
 #meow.STARTTASK(task)
 
-#meow.addscore("Anny", "Wang", 1)
+#meow.addscore("Anaya", "Qamar", 5)
 
 #note = '''Thanks for the enthusiasm y'all put into the dances! Before I send out the next task, PLEASE EMAIL ME with SUGGESTIONS for future tasks! (I'm running low on ideas)'''
 #meow.emailscores(note)
 
-meow.emailpair(task)
+#meow.emailpair(task)
