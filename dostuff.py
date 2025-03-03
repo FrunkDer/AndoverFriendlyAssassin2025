07bb93dd26dbb60ea5f3dd15045d934bf23cc1e6
Frank Hu <jinfanfrank@gmail.com>
2025-03-02 22:07:16 -0500

    Scores

--- dostuff.py
+++ dostuff.py
@@ -7,10 +7,10 @@ addscore(first, last, place)
 emailscores(note)
 '''
 
-task = '''For your last task of Winter term, take a video of you dapping your partner up. Best dap wins +5 extra points!'''
-meow.STARTTASK(task)
+# task = '''For your last task of Winter term, take a video of you dapping your partner up. Best dap wins +5 extra points!'''
+# meow.STARTTASK(task)
 
-#meow.addscore("Anaya", "Qamar", 5)
+#meow.addscore("Alex", "Small", 5)
 
 #note = '''Good luck on finals guys!! We'll have one more friendly assassin challenge for this term.'''
 #meow.emailscores(note)
